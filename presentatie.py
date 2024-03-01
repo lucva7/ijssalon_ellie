c18f28578bef39e86f248b41564181be471b1a62
Luc van Aalderen <lucvanaalderen@gmail.com>
2024-03-01 14:02:19 +0100

    Presentatie.py

---
+++ presentatie.py
@@ -0,0 +1,5 @@
+def presenteer(mijn_dict, totaal):
+    for key, value in mijn_dict.items():
+        print(f"{key} : {value} euro")
+    print("==========================")
+    print(f"Totaal : {totaal} euro")
